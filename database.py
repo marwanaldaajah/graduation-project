aa3760c7e512a5844bb956f09f57e56cbb5ed890
marwan alda'ajah <marwanaldaajah@gmail.com>
2023-01-04 08:59:36 +1200

    create databse

--- database.py
+++ database.py
@@ -1,28 +1,32 @@
 import pandas as pd
 
-def insert_row(file, row):
-  # Read the CSV file into a DataFrame
-  df = pd.read_csv(file)
+class Database:
+  def __init__(self, file):
+    self.file = file
 
-  # Insert the new row into the DataFrame
-  df = df.append(row, ignore_index=True)
+  def insert_row(self, row):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(self.file)
 
-  # Write the updated DataFrame to the CSV file
-  df.to_csv(file, index=False)
+    # Insert the new row into the DataFrame
+    df = df.append(row, ignore_index=True)
 
-def delete_row(file, index):
-  # Read the CSV file into a DataFrame
-  df = pd.read_csv(file)
+    # Write the updated DataFrame to the CSV file
+    df.to_csv(self.file, index=False)
 
-  # Delete the specified row from the DataFrame
-  df = df.drop(df.index[index])
+  def delete_row(self, index):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(self.file)
 
-  # Write the updated DataFrame to the CSV file
-  df.to_csv(file, index=False)
+    # Delete the specified row from the DataFrame
+    df = df.drop(df.index[index])
 
-def query(file, column, value):
-  # Read the CSV file into a DataFrame
-  df = pd.read_csv(file)
+    # Write the updated DataFrame to the CSV file
+    df.to_csv(self.file, index=False)
 
-  # Query the DataFrame and return the results
-  return df.query(f'{column} == {value}')
+  def query(self, column, value):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(self.file)
+
+    # Query the DataFrame and return the results
+    return df.query(f'{column} == {value}')
