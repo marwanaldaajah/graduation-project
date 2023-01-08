84b7f479012377fc487d23edb50303b708fc9398
marwan alda'ajah <marwanaldaajah@gmail.com>
2023-01-08 17:39:43 +1200

    add post fun

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socketserver
 
 class RequesHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        print(f'get fun: {self.path}')
         if self.path == '/':
             self.path='templates/search.html'
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
@@ -11,6 +12,9 @@ class RequesHandler(http.server.SimpleHTTPRequestHandler):
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
+    
+    def do_POST(self):
+        print(f'post fun: {self.path}')
 
 HOST='localhost'
 PORT=8000
