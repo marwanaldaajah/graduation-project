d04d8d8106266e683f75d6146a6261a0f0c90a3f
marwanaldaajah <marwanaldaajah@outlook.com>
2023-03-13 04:15:13 +0300

    app modify completed for first step

--- server.py
+++ server.py
@@ -1,27 +1,56 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pandas as pd
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/detect', methods=['POST'])
-def detect():
+
+@app.route('/upload', methods=['POST'])
+def upload():
     file = request.files['file']
-    inputData = pd.read_csv(file)
-    return render_template('detect.html', inputData=inputData)
+    filename = file.filename
+    file.save(f'uploads/{filename}')
+    return redirect(f'/detect?filename={filename}')
 
 
-@app.route('/result', methods=['POST'])
-def result():
-    result = request.form['result']
-    return render_template('result.html', result=result)
+@app.route('/detect')
+def detect():
+    filename = request.args.get('filename')
+    file_path = f'uploads/{filename}'
+
+    # Read the CSV data into a Pandas DataFrame
+    try:
+        df = pd.read_csv(file_path)
+        print(df)
+    except FileNotFoundError:
+        return render_template('error.html', message="File not found")
+    except pd.errors.ParserError:
+        return render_template('error.html', message="Error parsing the CSV file")
+
+    orgs = []
+    for index, row in df.iterrows():
+        name = row.get('Name')
+        website = row.get('Website')
+        country = row.get('Country')
+        if name and website and country:
+            orgs.append({
+                'Name': name,
+                'Website': website,
+                'Country': country
+            })
+
+    return render_template('detect.html', orgs=orgs)
 
 
-HOST='localhost'
-PORT=8000
+HOST = 'localhost'
+PORT = 8000
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
 if __name__ == '__main__':
     print(f"[STARTING] server is starting on PORT: {PORT}")
-    app.run(host=HOST,port=PORT)
+    app.run(debug=True, host=HOST, port=PORT)
