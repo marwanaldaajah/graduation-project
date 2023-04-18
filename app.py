b0e007282818de2a8984fb7e0b1691d7feab4c0c
marwanaldaajah <marwanaldaajah@outlook.com>
2023-04-18 07:14:52 +0300

    finish

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-import json
-from database import Database
+
+from matplotlib import pyplot as plt
+from config.database import Database
 import os
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
 import joblib
-from machine_learning import MachineLearning
+from config.machine_learning import MachineLearning
 
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'f1F@'
-
-
-key_file_name = 'file_name'
-key_input_columns = 'input_columns'
-key_output_columns = 'output_columns'
-key_train_size = 'train_size'
-key_test_size = 'test_size'
-key_model = 'model'
-key_scaler = 'key_scaler'
-key_accuracy = 'accuracy'
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -69,30 +59,57 @@ def train_model():
         if not train_size or not test_size:
             print('select train and testing size')
             return redirect(url_for("split_data"))
-    db.saveTrainSize(train_size)
-    db.saveTestSize(test_size)
+        db.saveTrainSize(train_size)
+        db.saveTestSize(test_size)
 
-    machineLearning = MachineLearning()
-    models = machineLearning.evaluate_models()
-    return render_template("training.html", models=models)
+        machineLearning = MachineLearning()
+        models = machineLearning.evaluate_models()
 
+        if not os.path.exists("result"):
+            os.makedirs("result")
+        for model in models:
+            joblib.dump(model['model'], f"result/{model['index']}.joblib")
+        return render_template("training.html", models=models)
+    return redirect(url_for("split_data"))
 
 @app.route("/model_result", methods=["GET", "POST"])
 def model_result():
     if request.method == 'POST':
-        model = request.form.get("model")
-    return render_template("model_results.html", model=model)
+        index = request.form.get("model")
+        model_path = f"result/{index}.joblib"
+        model = joblib.load(model_path)
 
+        machineLearning = MachineLearning()
+        model = machineLearning.evaluate_model(model)
+        plot_data = plot_results(model)
 
-@app.route('/save_model', methods=["GET", "POST"])
-def save_model_route():
-    if request.method == 'POST':
-        model = request.form.get("model")
-        index=model[6]
-        print(f'index: {index}')
-        # joblib.dump(model, 'model-result.joblib')
-        # predictions = model.predict('')
-    return render_template("model_results.html", model=model)
+    return render_template("model_results.html", model=model, plot_data=plot_data)
+
+import io
+import base64
+
+def plot_results(model):
+    # Extract model name, metrics, and scores from the model dictionary
+    model_name = str(model['name'])
+    accuracy = model['accuracy']
+    precision = model['precision']
+    recall = model['recall']
+    f1_score = model['f1']
+
+    # Create bar plot
+    fig, ax = plt.subplots()
+    ax.bar(['Accuracy', 'Precision', 'Recall', 'F1 Score'], [accuracy, precision, recall, f1_score])
+    ax.set_ylabel('Score')
+    ax.set_title(f'{model_name} Performance')
+
+    # Save plot to a buffer
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+
+    # Encode the buffer as a base64 string and embed it in the HTML page
+    plot_data = base64.b64encode(buf.getvalue()).decode('ascii')
+    return plot_data
 
 
 HOST = 'localhost'
@@ -102,88 +119,3 @@ if not os.path.exists('uploads'):
 if __name__ == '__main__':
     print(f"[STARTING] server is starting on PORT: {PORT}")
     app.run(debug=True, host=HOST, port=PORT)
-
-
-# @app.route("/train_model", methods=["GET", "POST"])
-# def train_model():
-#     db = Database()
-#     if request.method == "POST":
-#         train_size = request.form.get("train_size")
-#         test_size = request.form.get("test_size")
-#         if not train_size or not test_size:
-#             print('select train and testing size')
-#             return redirect(url_for("split_data"))
-#         else:
-#             db.saveTestSize(train_size)
-#             db.saveTestSize(test_size)
-
-#     df = db.readFileScv()
-#     input_columns = db.readInputColumn()
-#     output_columns = db.readOutputColumns()
-#     train_size = db.readTrainSize()
-#     test_size = db.readTestSize()
-
-#     # Handle missing values
-#     df.dropna(inplace=True)
-#     # print(f'df: {df}')
-#     # Convert categorical data to numerical data
-#     cat_columns = df.select_dtypes(include=['object']).columns
-#     df[cat_columns] = df[cat_columns].apply(
-#         lambda x: x.astype('category').cat.codes)
-#     # print(f'df: {df}')
-
-#     # Preprocess the data
-#     X = df[input_columns]
-#     y = df[output_columns]
-
-#     # Split the data into training and testing sets
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         X, y, train_size=train_size, test_size=test_size, random_state=42)
-
-#     # Normalize the data
-#     scaler = StandardScaler()
-#     X_train = scaler.fit_transform(X_train)
-#     X_test = scaler.transform(X_test)
-
-#     # Check if there are negative values in X_train and handle them
-#     if np.any(X_train < 0).any():
-#         # Add the minimum value of X_train to make all values non-negative
-#         X_train += abs(np.min(X_train))
-
-#     # Check if there are negative values in X_test and handle them
-#     if np.any(X_test < 0).any():
-#         # Add the minimum value of X_test to make all values non-negative
-#         X_test += abs(np.min(X_test))
-
-#     # Flatten y_train and y_test to one-dimensional arrays
-#     y_train = np.ravel(y_train)
-#     y_test = np.ravel(y_test)
-
-#     # Train and evaluate models
-#     models = []
-#     i = 0
-#     try:
-#         for name, model in machineLearningModels:
-#             print(f'i: {i} , test model name: {name}')
-#             model.fit(X_train, y_train)
-#             y_pred = model.predict(X_test)
-#             # calculate additional metrics as needed
-#             precision = precision_score(y_test, y_pred, average='micro')
-#             recall = recall_score(y_test, y_pred, average='micro')
-#             f1 = f1_score(y_test, y_pred, average='micro')
-#             accuracy = accuracy_score(y_test, y_pred)
-#             # create a dictionary for this model's results
-#             model_results = {
-#                 'index': i,
-#                 'name': name,
-#                 'accuracy': accuracy,
-#                 'precision': precision,
-#                 'recall': recall,
-#                 'f1': f1
-#             }
-#             models.append(model_results)
-#             i = i + 1
-#     except Exception as e:
-#         print(f'Error occurred: {e} model: {name}')
-
-#     return render_template("training.html", models=models)
--- model/model.py
+++ model/model.py
@@ -1,8 +1,9 @@
 class Model:
-    def __init__(self, name, accuracy, precision, recall, f1):
+    def __init__(self,index, name, accuracy, precision, recall, f1,model):
+        self.index = index
         self.name = name
         self.accuracy = accuracy
         self.precision = precision
         self.recall = recall
         self.f1 = f1
-
+        self.model=model
